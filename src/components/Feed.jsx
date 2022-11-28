d3399755a6d5bb19ee39ec64cbdb5ee27be54417
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-27 22:10:33 -0500

    now its all scratched

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,30 +2,3 @@ import { useState, useEffect } from "react"
 import { Button } from "antd"
 import UploadModal from "./UploadModal"
 import Post from "./Post"
-
-export default function Feed() {
-  const [photoList, setPhotoList] = useState()
-  const [showUpload, setShowUpload] =  useState(false)
-  useEffect(() => {
-    fetch('https://corwins-final-api.web.app')
-    .then(results => results.json())
-    .then(data => setPhotoList(data))
-    .catch(alert)
-  }, [setPhotoList])
-    return (
-       
-          <section className="photo-feed">
-           {!photoList
-           ? <p>Loading...</p>
-           : photoList.map(post => (
-            <Post setPhotoList={setPhotoList} post={post} key={post.photoId} />
-           ))
-          }
-          {showUpload ? <UploadModal setPhotoList={setPhotoList} setShowUpload={setShowUpload}/> : null}
-        
-          <Button onClick={() => setShowUpload(true)} 
-          className="fab" type="primary" 
-          shape="circle" size="large">+</Button>
-          </section>
-    )   
-    }
